ed6d628f4ca722f528e28caa2e8f4cf585dc95a8
ftcnico <57717425+ftcnico@users.noreply.github.com>
2019-11-15 05:25:23 -0300

    Update new project python.pl

--- new project python.pl
+++ new project python.pl
@@ -1,14 +1,30 @@
-a=1
-def f();
-a=2
-printf(a)
-f()
-print(a)
-f():
-a=1
-def f():
-global a 
-a=2
-print(a)
-f()
-print(a)
+# a=1
+#def f():
+#    a=2
+#    print(a)
+
+#f()
+#print(a)
+
+#a=1
+#def f(); 
+#     global a
+#     a=2
+#     print(a)
+#f()
+#print(a)
+
+
+    
+
+def greet(name):
+    print("Hello, {0}!".format(name))
+print("What's your name?")
+name = input()
+greet(name)
+
+def greet(age):
+    print("Hello, {0}!".format(age))
+print("What's your age?")
+age = input()
+greet(age)
